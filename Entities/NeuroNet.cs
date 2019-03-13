d7f6030001d80ad8ddcce5efbdef780a43cc1588
Jambo <jambotoyou@gmail.com>
2019-03-14 00:02:09 +0400

    1

--- Entities/NeuroNet.cs
+++ Entities/NeuroNet.cs
@@ -1,20 +1,38 @@
+using System;
+
 namespace NeuroNet.Entities
 {
-    public class NeuroNet
-    {
-        public double[][] Neurons { get; set; }
-        public double[][,] Weights { get; set; }
+	public class NeuroNet
+	{
+		public double[][] Neurons { get; set; }
+		public double[][,] Weights { get; set; }
+		public Func<double, double> ActivationFunc { get; set; }
 
-        public NeuroNet(int[] neuronsCount)
-        {
-            Neurons = new double[neuronsCount.Length][];
-            Weights = new double[neuronsCount.Length][,];
+		public NeuroNet(int[] neuronsCount)
+		{
+			var rand = new Random();
+			Neurons = new double[neuronsCount.Length][];
+			Weights = new double[neuronsCount.Length][,];
 
-            for (int layerIdx = 0; layerIdx < neurons.Length; layerIdx++)
-            {
-                Neurons[layerIdx] = new double[neuronsCount[layerIdx]];
-                Weights[layerIdx] = new double[neuronsCount[layerIdx]];
-            }
-        }
-    }
+			Neurons[0] = new double[neuronsCount[0]];
+			for (int layerIdx = 1; layerIdx < neurons.Length; layerIdx++)
+			{
+				Neurons[layerIdx] = new double[neuronsCount[layerIdx]];
+				Weights[layerIdx] = new double
+					[
+						neuronsCount[layerIdx],
+						neuronsCount[layerIdx - 1]
+					];
+				
+				for (int neuronIdx = 0; neuronIdx < neuronsCount[layerIdx]; neuronIdx++)
+					for (int synapsIdx = 0; synapsIdx < neuronsCount[layerIdx - 1]; synapsIdx++)
+						Weights[layerIdx][neuronIdx, synapsIdx] = rand.NextDouble() - 0.5;
+			}
+		}
+
+		public void InitInput(double[] inputs)
+		{
+
+		}
+	}
 }
