d1d6b89faf50044621c7aa2296db1940b6a5129a
AndreyTyu <jambotoyou@gmail.com>
2019-04-16 01:57:14 +0300

    Renamed ImageNormalizer project to ImageHelper

--- ImageHelper/ImageNormalizer.cs
+++ ImageHelper/ImageNormalizer.cs
@@ -1,7 +1,7 @@
 using System;
 using System.Drawing;
 
-namespace ImageNormalizer
+namespace ImageHelper
 {
 	public static class ImageNormalizer
 	{
--- ImageNormalizer/Entities/NormalizedMap.cs
+++
@@ -1,9 +0,0 @@
-using System;
-
-namespace ImageNormalizer.Entities
-{
-	public class NormalizedMap
-	{
-		
-	}
-}
